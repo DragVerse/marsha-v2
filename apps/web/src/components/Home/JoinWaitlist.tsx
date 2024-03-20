fb7e6c5a4b4622bf9aa67b311a65fe4c153f539c
salti <justsoantsy@gmail.com>
2024-03-20 04:11:14 -0600

    Updating Home Cards

--- apps/web/src/components/Home/JoinWaitlist.tsx
+++ apps/web/src/components/Home/JoinWaitlist.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const JoinWaitlist = () => {
   return (
-    <div className="tape-border rounded-large ultrawide:h-[400px] relative flex h-[350px] w-[500px] flex-none overflow-hidden  sm:w-[300px] md:w-[300px]">
+<div className="tape-border rounded-large ultrawide:h-[400px] relative flex h-[350px] w-full sm:w-[300px] lg:w-[500px] flex-none overflow-hidden">
       <div className="bg-brand-250 absolute inset-0 h-full w-full" />
       <div className="from-brand-250 absolute inset-0 h-full w-full bg-gradient-to-b to-transparent" />
       <div className="ultrawide:p-8 relative flex h-full flex-col justify-end space-y-4 p-4 text-left text-white md:p-6">
--- apps/web/src/components/Home/WelcomeAlert.tsx
+++ apps/web/src/components/Home/WelcomeAlert.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link'
 
 const WelcomeAlert = () => {
   return (
-    <div className="tape-border rounded-large ultrawide:h-[400px] relative flex h-[350px] w-[500px] flex-none overflow-hidden  sm:w-[300px] md:w-[400px]">
-      <div className="bg-brand-950 absolute inset-0 h-full w-full" />
+    <div className="tape-border rounded-large ultrawide:h-[400px] relative flex h-[350px] w-full sm:w-[300px] lg:w-[500px] flex-none overflow-hidden">
       <div className="from-brand-900 absolute inset-0 h-full w-full bg-gradient-to-b to-transparent" />
       <div className="ultrawide:p-8 relative flex h-full flex-col justify-end space-y-4 p-4 text-left text-white md:p-6">
         <div className="text-3xl font-bold">Welcome to {TAPE_APP_NAME}</div>
