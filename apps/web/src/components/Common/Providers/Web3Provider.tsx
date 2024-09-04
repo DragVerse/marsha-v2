e024fe0f9358f7ac3ad4eeb0e9d86b93b6490a2e
salti <justsoantsy@gmail.com>
2024-09-04 00:49:41 -0700

    Returning to previous setting

--- apps/web/src/components/Common/Providers/Web3Provider.tsx
+++ apps/web/src/components/Common/Providers/Web3Provider.tsx
@@ -1,5 +1,4 @@
-import { PRIVY_APP_ID, WC_PROJECT_ID } from '@dragverse/constants'
-import { PrivyProvider } from '@privy-io/react-auth'
+import { WC_PROJECT_ID } from '@dragverse/constants'
 import { type FC, type ReactNode } from 'react'
 import { createConfig, http, WagmiProvider } from 'wagmi'
 import { polygon, polygonAmoy } from 'wagmi/chains'
@@ -27,22 +26,7 @@ type Props = {
 }
 
 const Web3Provider: FC<Props> = ({ children }) => {
-  return (
-    <PrivyProvider
-      appId={PRIVY_APP_ID}
-      config={{
-        loginMethods: ['email', 'wallet'],
-        appearance: {
-          theme: 'light',
-          accentColor: '#676FFF',
-          logo: 'https://your-logo-url.com'
-        },
-        supportedChains: [polygon, polygonAmoy]
-      }}
-    >
-      <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
-    </PrivyProvider>
-  )
+  return <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
 }
 
 export default Web3Provider
